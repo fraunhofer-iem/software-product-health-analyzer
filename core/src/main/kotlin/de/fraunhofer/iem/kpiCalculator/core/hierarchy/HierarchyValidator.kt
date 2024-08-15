e635293e63fe393d36d2a5fe25f7c84820d0e587
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-08-15 09:50:33 +0200

    feature: added logging and internalIsValid to hierarchy validation. - implemented internalIsValid for MaximumKPICalculationStrategy and AggregationKPICalculationStrategy

--- core/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/core/hierarchy/HierarchyValidator.kt
+++ core/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/core/hierarchy/HierarchyValidator.kt
@@ -10,12 +10,15 @@
 package de.fraunhofer.iem.kpiCalculator.core.hierarchy
 
 import de.fraunhofer.iem.kpiCalculator.model.kpi.hierarchy.KpiHierarchy
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 object HierarchyValidator {
     //TODO: return type will most likely be changed to a complex type comparably to KpiCalculationResult
     fun isValid(kpiHierarchy: KpiHierarchy, strict: Boolean = false): Boolean {
         // TODO: check the schema version and build a schema specific validation if necessary
-
+        logger.info { "Started KPI hierarchy validation with strict mode $strict." }
         val root = kpiHierarchy.rootNode
 
         // handle empty hierarchies
@@ -23,11 +26,10 @@ object HierarchyValidator {
             if (strict) {
                 return false
             } else {
-                // TODO: log a warning here, empty hierarchies might be allowed but are unusual
+                logger.warn { "Provided KPI hierarchy is empty." }
             }
         }
 
         return true
     }
-
 }
--- core/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/core/strategy/AggregationKPICalculationStrategy.kt
+++ core/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/core/strategy/AggregationKPICalculationStrategy.kt
@@ -12,7 +12,9 @@ package de.fraunhofer.iem.kpiCalculator.core.strategy
 import de.fraunhofer.iem.kpiCalculator.model.kpi.KpiStrategyId
 import de.fraunhofer.iem.kpiCalculator.model.kpi.hierarchy.KpiCalculationResult
 import de.fraunhofer.iem.kpiCalculator.model.kpi.hierarchy.KpiNode
+import io.github.oshai.kotlinlogging.KotlinLogging
 
+private val logger = KotlinLogging.logger {}
 
 internal object AggregationKPICalculationStrategy : BaseKpiCalculationStrategy() {
 
@@ -54,7 +56,18 @@ internal object AggregationKPICalculationStrategy : BaseKpiCalculationStrategy()
         return KpiCalculationResult.Success(score = aggregation)
     }
 
+    /**
+     * There is no validity requirement for this strategy.
+     */
     override fun internalIsValid(node: KpiNode, strict: Boolean): Boolean {
-        TODO("Not yet implemented")
+
+        if (node.edges.size == 1) {
+            logger.warn {
+                "Maximum KPI calculation strategy for node $node is planned" +
+                        "for a single child."
+            }
+        }
+
+        return true
     }
 }
