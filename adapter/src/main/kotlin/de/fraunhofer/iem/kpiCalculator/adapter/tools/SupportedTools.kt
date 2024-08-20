fc82fed76d51e5a053dd9317dd6786e53552864a
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-08-20 18:35:17 +0200

    doc: added copyright notice to all files.

--- adapter/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/adapter/tools/SupportedTools.kt
+++ adapter/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/adapter/tools/SupportedTools.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2024 Fraunhofer IEM. All rights reserved.
+ *
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ *
+ * SPDX-License-Identifier: MIT
+ * License-Filename: LICENSE
+ */
+
 package de.fraunhofer.iem.kpiCalculator.adapter.tools
 
 enum class SupportedTool {
@@ -8,7 +17,7 @@ enum class SupportedTool {
             try {
                 return SupportedTool.valueOf(name)
             } catch (e: IllegalArgumentException){
-                throw ToolNotFoundException("Unable to find tool result adapter for tool '$name'.");
+                throw ToolNotFoundException("Unable to find tool result adapter for tool '$name'.")
             }
         }
     }
