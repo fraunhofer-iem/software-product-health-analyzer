ebec69a17ede55274cb2a14904d6a2ec60b436a8
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-08-21 16:44:46 +0200

    chore: added copyright to new files

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2024 Fraunhofer IEM. All rights reserved.
+ *
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ *
+ * SPDX-License-Identifier: MIT
+ * License-Filename: LICENSE
+ */
+
 rootProject.name = "buildSrc"
 
 dependencyResolutionManagement {
