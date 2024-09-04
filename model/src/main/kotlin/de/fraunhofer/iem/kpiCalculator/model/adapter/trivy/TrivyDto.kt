78952c2254bbd3291cc7efd721904081775a0bf4
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-09-04 17:05:44 +0200

    fix: fixed incorrect refactoring of TrivyDto.
    - added explicit SerialNames to TrivyDto to not have capital case variable names

--- model/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/model/adapter/trivy/TrivyDto.kt
+++ model/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/model/adapter/trivy/TrivyDto.kt
@@ -10,24 +10,39 @@
 package de.fraunhofer.iem.kpiCalculator.model.adapter.trivy
 
 import de.fraunhofer.iem.kpiCalculator.model.adapter.vulnerability.VulnerabilityDto
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
 
 data class TrivyDto(val vulnerabilities: Collection<VulnerabilityDto>)
 
-@Serializable data class TrivyDtoV1(val vulnerabilities: List<TrivyVulnerabilityDto> = listOf())
+@Serializable
+data class TrivyDtoV1(
+    @SerialName("Vulnerabilities") val vulnerabilities: List<TrivyVulnerabilityDto> = listOf()
+)
 
-@Serializable data class TrivyDtoV2(val results: List<Result> = listOf(), val schemaVersion: Int)
+@Serializable
+data class TrivyDtoV2(
+    @SerialName("Results") val results: List<Result> = listOf(),
+    @SerialName("SchemaVersion") val schemaVersion: Int,
+)
 
-@Serializable data class Result(val vulnerabilities: List<TrivyVulnerabilityDto> = listOf())
+@Serializable
+data class Result(
+    @SerialName("Vulnerabilities") val vulnerabilities: List<TrivyVulnerabilityDto> = listOf()
+)
 
 @Serializable
 data class TrivyVulnerabilityDto(
     // NB: Because the names of its inner elements are not fixed, this needs to be a JsonObject.
     // This way we can iterate over those when required. Their type is always CVSSData.
-    val cvss: JsonObject?,
-    val vulnerabilityID: String,
-    val pkgID: String,
+    @SerialName("CVSS") val cvss: JsonObject?,
+    @SerialName("VulnerabilityID") val vulnerabilityID: String,
+    @SerialName("PkgID") val pkgID: String,
 )
 
-@Serializable data class CVSSData(val v2Score: Double?, val v3Score: Double?)
+@Serializable
+data class CVSSData(
+    @SerialName("V2Score") val v2Score: Double?,
+    @SerialName("V3Score") val v3Score: Double?,
+)
