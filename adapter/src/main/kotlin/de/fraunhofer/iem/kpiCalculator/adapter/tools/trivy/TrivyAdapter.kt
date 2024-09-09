78c2354fb6c783a63e5b8038a89d4c32d79f223c
AnakinRaW <8994294+AnakinRaW@users.noreply.github.com>
2024-09-09 13:31:44 +0200

    fix: add overload

--- adapter/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/adapter/tools/trivy/TrivyAdapter.kt
+++ adapter/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/adapter/tools/trivy/TrivyAdapter.kt
@@ -28,8 +28,12 @@ object TrivyAdapter {
         explicitNulls = false
     }
 
+    fun transformDataToKpi(data: Collection<TrivyDto>): Collection<AdapterResult> {
+        return CveAdapter.transformDataToKpi(data.flatMap { it.vulnerabilities })
+    }
+
     fun transformDataToKpi(data: TrivyDto): Collection<AdapterResult> {
-        return CveAdapter.transformDataToKpi(data.vulnerabilities)
+        return transformDataToKpi(listOf(data))
     }
 
     @OptIn(ExperimentalSerializationApi::class)
@@ -45,7 +49,7 @@ object TrivyAdapter {
         if (schemaVersion == 2) return parseV2(json)
 
         throw UnsupportedOperationException(
-                "Trivy results for schema version '$schemaVersion' are currently not supported.",
+            "Trivy results for schema version '$schemaVersion' are currently not supported."
         )
     }
 
