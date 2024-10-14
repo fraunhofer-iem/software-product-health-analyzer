f88e4cfcd623f87d9a74b3eb364ecf1d4435fb91
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-10-14 09:16:59 +0200

    chore: added logs to KpiCalculator

--- core/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/core/KpiCalculator.kt
+++ core/src/main/kotlin/de/fraunhofer/iem/kpiCalculator/core/KpiCalculator.kt
@@ -19,15 +19,20 @@ import de.fraunhofer.iem.kpiCalculator.model.kpi.RawValueKpi
 import de.fraunhofer.iem.kpiCalculator.model.kpi.hierarchy.KpiCalculationResult
 import de.fraunhofer.iem.kpiCalculator.model.kpi.hierarchy.KpiHierarchy
 import de.fraunhofer.iem.kpiCalculator.model.kpi.hierarchy.KpiResultHierarchy
+import io.github.oshai.kotlinlogging.KotlinLogging
 
 object KpiCalculator {
-    // XXX: Setup Logger
+
+    private val logger = KotlinLogging.logger {}
 
     fun calculateKpis(
         hierarchy: KpiHierarchy,
         rawValueKpis: List<RawValueKpi>,
         strict: Boolean = false,
     ): KpiResultHierarchy {
+        logger.info {
+            "Running KPI calculation on $hierarchy and $rawValueKpis with strict mode=$strict"
+        }
         val root = KpiHierarchyNode.from(hierarchy.rootNode, rawValueKpis)
 
         depthFirstTraversal(root) { it.result = calculateKpi(it, strict) }
@@ -35,10 +40,12 @@ object KpiCalculator {
         return KpiResultHierarchy.create(KpiHierarchyNode.to(root))
     }
 
+    /** Selects and executes the Kpi strategy related to the given node */
     internal fun calculateKpi(
         node: KpiHierarchyNode,
         strict: Boolean = false,
     ): KpiCalculationResult {
+        logger.info { "Running KPI calculation on $node" }
         val result =
             when (node.kpiStrategyId) {
                 KpiStrategyId.RAW_VALUE_STRATEGY -> node.result
@@ -52,6 +59,7 @@ object KpiCalculator {
                 KpiStrategyId.MAXIMUM_STRATEGY ->
                     MaximumKPICalculationStrategy.calculateKpi(node.hierarchyEdges, strict)
             }
+        logger.info { "KPI calculation result $result" }
         return result
     }
 }
