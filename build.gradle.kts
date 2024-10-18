d19d681ceb2f2e53d19ae63eb5a9995233772f27
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-10-18 15:00:58 +0200

    deps: version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@
  */
 
 plugins {
-    id("com.github.jmongard.git-semver-plugin") version "0.12.10"
+    id("com.github.jmongard.git-semver-plugin") version "0.12.11"
     id("com.github.ben-manes.versions") version "0.51.0"
 }
 
