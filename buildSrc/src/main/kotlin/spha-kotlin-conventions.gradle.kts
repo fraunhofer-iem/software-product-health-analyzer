057bb9dac5784c8db15500de93622e0746d62d54
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-10-24 09:48:14 +0200

    ci: we don't need to explicitly set repository information for maven repository

--- buildSrc/src/main/kotlin/spha-kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/spha-kotlin-conventions.gradle.kts
@@ -73,20 +73,6 @@ mavenPublishing {
             url = "https://github.com/fraunhofer-iem/software-product-health-analyzer"
         }
     }
-
-    repositories {
-        maven {
-            name = "mavenCentral"
-            val releasesRepoUrl =
-                "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-            url =
-                uri(
-                    if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl
-                    else releasesRepoUrl
-                )
-        }
-    }
 }
 
 kotlin {
