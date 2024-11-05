5f9ae19eff8046ff2d30fca03b4ea930511cfa13
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2024-11-05 13:54:30 +0100

    doc: added severity explanation to TrivyVulnerabilityDto

--- model/src/main/kotlin/de/fraunhofer/iem/spha/model/adapter/trivy/TrivyDto.kt
+++ model/src/main/kotlin/de/fraunhofer/iem/spha/model/adapter/trivy/TrivyDto.kt
@@ -40,6 +40,14 @@ data class TrivyVulnerabilityDto(
     @SerialName("VulnerabilityID") val vulnerabilityID: String,
     @SerialName("InstalledVersion") val installedVersion: String,
     @SerialName("PkgName") val pkgName: String,
+    /**
+     * | Base Score Range | Severity |
+     * |------------------|----------|
+     * | 0.1-3.9          | Low      |
+     * | 4.0-6.9          | Medium   |
+     * | 7.0-8.9          | High     |
+     * | 9.0-10.0         | Critical |
+     */
     @SerialName("Severity") val severity: String,
 )
 
